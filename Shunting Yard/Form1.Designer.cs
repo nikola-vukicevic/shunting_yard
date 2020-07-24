395e503fc0c3e8dbe819a44de44825e959f97b5c
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2020-07-24 21:55:00 +0200

    Add files via upload

---
+++ Shunting Yard/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Shunting_Yard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbox_a = new System.Windows.Forms.TextBox();
+            this.tbox_b = new System.Windows.Forms.TextBox();
+            this.tbox_c = new System.Windows.Forms.TextBox();
+            this.tbox_d = new System.Windows.Forms.TextBox();
+            this.tbox_e = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Prebaci izraz u postfiksnu notaciju i izracunaj vrednost";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(460, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Postfiksna notacija:";
+            // 
+            // tbox_a
+            // 
+            this.tbox_a.Location = new System.Drawing.Point(372, 56);
+            this.tbox_a.Name = "tbox_a";
+            this.tbox_a.Size = new System.Drawing.Size(100, 20);
+            this.tbox_a.TabIndex = 3;
+            this.tbox_a.Text = "0.0";
+            // 
+            // tbox_b
+            // 
+            this.tbox_b.Location = new System.Drawing.Point(372, 85);
+            this.tbox_b.Name = "tbox_b";
+            this.tbox_b.Size = new System.Drawing.Size(100, 20);
+            this.tbox_b.TabIndex = 3;
+            this.tbox_b.Text = "0.0";
+            // 
+            // tbox_c
+            // 
+            this.tbox_c.Location = new System.Drawing.Point(372, 111);
+            this.tbox_c.Name = "tbox_c";
+            this.tbox_c.Size = new System.Drawing.Size(100, 20);
+            this.tbox_c.TabIndex = 3;
+            this.tbox_c.Text = "0.0";
+            // 
+            // tbox_d
+            // 
+            this.tbox_d.Location = new System.Drawing.Point(372, 137);
+            this.tbox_d.Name = "tbox_d";
+            this.tbox_d.Size = new System.Drawing.Size(100, 20);
+            this.tbox_d.TabIndex = 3;
+            this.tbox_d.Text = "0.0";
+            // 
+            // tbox_e
+            // 
+            this.tbox_e.Location = new System.Drawing.Point(372, 163);
+            this.tbox_e.Name = "tbox_e";
+            this.tbox_e.Size = new System.Drawing.Size(100, 20);
+            this.tbox_e.TabIndex = 3;
+            this.tbox_e.Text = "0.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(350, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "a:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(350, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "b:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(350, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "c:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(350, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "d:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(350, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "e:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 134);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 19);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Rezultat:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 164);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 19);
+            this.label9.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 202);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbox_e);
+            this.Controls.Add(this.tbox_d);
+            this.Controls.Add(this.tbox_c);
+            this.Controls.Add(this.tbox_b);
+            this.Controls.Add(this.tbox_a);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(505, 240);
+            this.MinimumSize = new System.Drawing.Size(505, 240);
+            this.Name = "Form1";
+            this.Text = "Shunting Yard";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbox_a;
+        private System.Windows.Forms.TextBox tbox_b;
+        private System.Windows.Forms.TextBox tbox_c;
+        private System.Windows.Forms.TextBox tbox_d;
+        private System.Windows.Forms.TextBox tbox_e;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Shunting Yard/Form1.cs
@@ -0,0 +1,189 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// Shunting yard - implementacija (12.11.2015.)                               //
+// Autor: Nikola Vukicevic (projekat za II godinu)                            //
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Shunting_Yard
+{
+    public partial class Form1 : Form
+    {
+        Double var_a, var_b, var_c, var_d, var_e;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dodajPlusMinus(char c, Stack<char> stek, Queue<char> red)
+        {
+            if (stek.Peek() == '(' || stek.Peek() == '?')
+            {
+                stek.Push(c);
+            }
+            else
+            {
+                while (stek.Peek() == '+' || stek.Peek() == '-' ||
+                       stek.Peek() == '*' || stek.Peek() == '/')
+                {
+                    red.Enqueue(stek.Pop());
+                }
+                stek.Push(c);
+            }
+         }
+
+        private void dodajPutaKroz(char c, Stack<char> stek, Queue<char> red)
+        {
+            if (stek.Peek() == '(' || stek.Peek() == '?' ||
+                stek.Peek() == '+' || stek.Peek() == '-')
+            {
+                stek.Push(c);
+            }
+            else
+            {
+                while (stek.Peek() == '/' || stek.Peek() == '*')
+                {
+                    red.Enqueue(stek.Pop());
+                }
+                stek.Push(c);
+            }
+        }
+
+        private void dodajZatvorenuZagradu(char c, Stack<char> stek, Queue<char> red)
+        {
+            while (stek.Peek() != '(')
+            {
+                red.Enqueue(stek.Pop());
+            }
+            stek.Pop();
+        }
+
+        private string Parse(string s)
+        {
+            Int32 i, d = s.Length;
+            String str = "";
+            Queue<char> red = new Queue<char>();
+            Stack<char> stek = new Stack<char>();
+            stek.Push('?');
+            
+            for (i = 0; i < d; i++)
+            {
+                char c = s[i];
+
+                if (c >= 97 && c <= 122)
+                {
+                    red.Enqueue(c);
+                }
+                else
+                {
+                    switch (c)
+                    {
+                        case '(': stek.Push(c); break;
+                        case '+':
+                        case '-': dodajPlusMinus(c, stek, red); break;
+                        case '*':
+                        case '/': dodajPutaKroz(c, stek, red); break;
+                        case ')': dodajZatvorenuZagradu(c, stek, red); break;
+                        default: break;
+                     }
+                }
+            }
+
+            while (stek.Peek() != '?') red.Enqueue(stek.Pop());
+            ProcenaVrednosti(red);
+            while (red.Count > 0) str += red.Dequeue().ToString();
+            return str;
+        }
+
+        private void ProcenaVrednosti(Queue<char> red)
+        {
+            Stack<Double> stek = new Stack<Double>();
+            char[] tokeni = red.ToArray();
+            Int32 i, d = tokeni.Length;
+            
+            for (i = 0; i < d;i++ )
+            {
+                char znak = tokeni[i];
+
+                if (znak >= 97 && znak <= 122)
+                {
+                    switch (znak)
+                    {
+                        case 'a': stek.Push(var_a); break;
+                        case 'b': stek.Push(var_b); break;
+                        case 'c': stek.Push(var_c); break;
+                        case 'd': stek.Push(var_d); break;
+                        case 'e': stek.Push(var_e); break;
+                        default: MessageBox.Show("Uneli ste neodgovarajucu promenljivu!"); return;
+                    }
+                }
+                else
+                {
+                    Double Op2 = stek.Pop(), Op1 = stek.Pop();
+                    switch (znak)
+                    {
+                        case '+': stek.Push(Op1 + Op2); break;
+                        case '-': stek.Push(Op1 - Op2); break;
+                        case '*': stek.Push(Op1 * Op2); break;
+                        case '/': stek.Push(Op1 / Op2); break;
+                        default: break;
+                    }
+                }
+            }
+
+            Double rez = stek.Pop();
+            label9.Text = rez.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(tbox_a.Text, out var_a))
+            {
+                MessageBox.Show("Greška pri učitavanju promenljive a!");
+                return;
+            }
+            
+            if (!Double.TryParse(tbox_b.Text, out var_b))
+            {
+                MessageBox.Show("Greška pri učitavanju promenljive b!");
+                return;
+            }
+
+            if (!Double.TryParse(tbox_c.Text, out var_c))
+            {
+                MessageBox.Show("Greška pri učitavanju promenljive c!");
+                return;
+            }
+
+            if (!Double.TryParse(tbox_d.Text, out var_d))
+            {
+                MessageBox.Show("Greška pri učitavanju promenljive d!");
+                return;
+            }
+
+            if (!Double.TryParse(tbox_e.Text, out var_e))
+            {
+                MessageBox.Show("Greška pri učitavanju promenljive e!");
+                return;
+            }
+
+            label1.Text = Parse(textBox1.Text);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                textBox1.Text = label1.Text = "";
+            }
+        }
+    }
+}
